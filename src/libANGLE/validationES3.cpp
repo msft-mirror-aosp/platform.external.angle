9f19b8ca2fb36c40f38fbdaf63e3d0931552f504
Yahan Zhou <yahan@google.com>
2017-08-15 14:31:07 -0700

    Emulator boot animation in angle_indirect
    There is use cases in emulator where the read framebuffer format is not
    compatible with destination texture formats in glCopyTexSubImage.
    
    We remove some of the validation code. We are already doing similar
    thing in GLES2. Please revert this CL and the GLES2 CL when we have a
    proper fix in the emulator code.
    
    Link to GLES2 part.
    
    https://android.googlesource.com/platform/external/angle/+/emu-master-dev/src/libANGLE/validationES2.cpp#575
    
    BUG: 64540066
    Change-Id: I1210480e529d50d0e49a405d0c886671d0dd08ea

--- src/libANGLE/validationES3.cpp
+++ src/libANGLE/validationES3.cpp
@@ -725,6 +725,8 @@ static CopyConversionSet BuildValidES3CopyTexImageCombinations()
     set.insert(CopyConversion(GL_RGB_INTEGER, GL_RGBA_INTEGER));
     set.insert(CopyConversion(GL_RGBA_INTEGER, GL_RGBA_INTEGER));
 
+    // Necessary for emulator, BUG: 64540066
+    set.insert(CopyConversion(GL_RGBA, GL_RGB));
     return set;
 }
 
@@ -845,9 +847,12 @@ static bool IsValidES3CopyTexImageCombination(GLenum textureInternalFormat, GLen
                 !EqualOrFirstZero(textureInternalFormatInfo.greenBits,
                                   sourceEffectiveFormat->greenBits) ||
                 !EqualOrFirstZero(textureInternalFormatInfo.blueBits,
-                                  sourceEffectiveFormat->blueBits) ||
-                !EqualOrFirstZero(textureInternalFormatInfo.alphaBits,
-                                  sourceEffectiveFormat->alphaBits))
+                                  sourceEffectiveFormat->blueBits)
+                // Emulator does not work well with the following rule
+                // BUG: 64540066
+                //|| !EqualOrFirstZero(textureInternalFormatInfo.alphaBits,
+                //                sourceEffectiveFormat->alphaBits)
+                )
             {
                 return false;
             }
