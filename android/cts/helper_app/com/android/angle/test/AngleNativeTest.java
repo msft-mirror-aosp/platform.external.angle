42a37975ecae45d44c87267d9ff9ce1074da7381
Tim Van Patten <timvp@google.com>
2025-07-17 16:23:47 -0600

    Write log files to /sdcard
    Move the AngleNativeTest logging from /sdcard/Download to /sdcard. When
    accessing the files via adb, this maps to: /data/media/[0|10].
    
    This makes life easier when kicking off the tests and accessing the log
    and results files with hostside runner.
    
    This CL also cleans up any old out.txt files, so the logs are always
    fresh and relevant to the current test invocation.
    
    Bug: b/279980674
    Test: m AngleEnd2EndTestsHelper
    Change-Id: Ied8ba51ea90aef193c04986bb335afdeab8a062d

--- android/cts/helper_app/com/android/angle/test/AngleNativeTest.java
+++ android/cts/helper_app/com/android/angle/test/AngleNativeTest.java
@@ -9,12 +9,18 @@ package com.android.angle.test;
 
 import android.app.NativeActivity;
 import android.os.Handler;
+import android.util.Log;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public final class AngleNativeTest extends NativeActivity {
     private static final String TAG = "NativeTest";
-    private static final String OUTPUT_DIR = "/sdcard/Download";
+    // On the device, maps to: /data/media/[0|10]
+    // See: https://source.android.com/docs/devices/admin/multi-user-testing
+    private static final String OUTPUT_DIRECTORY = "/sdcard";
     private static final String STDOUT_FILENAME = "out.txt";
 
     static {
@@ -22,15 +28,11 @@ public final class AngleNativeTest extends NativeActivity {
         System.loadLibrary("angle_end2end_tests__library");
     }
 
-    String getStdoutFilePath() {
-        return Paths.get(OUTPUT_DIR, STDOUT_FILENAME).toString();
-    }
-
     String getCommandLineFlags() {
         String commandLineFlags = "";
-        // Generate an output.json file. Translates to
-        // /data/media/<userID>/Download on the device.
-        commandLineFlags += "--results-directory=" + OUTPUT_DIR;
+        // Generate an output.json file. On the device, translates to: /data/media/[0|10]
+        commandLineFlags += "--results-directory=" + OUTPUT_DIRECTORY;
+
         // TODO(b/279980674): Remove filter and Enable all tests, filtered by the expectations file.
         // We want to run at least one test to exercise things, and this one is small and passing.
         commandLineFlags += " --gtest_filter=MemorySizeTest.BasicUsageTexture/* ";
@@ -41,13 +43,25 @@ public final class AngleNativeTest extends NativeActivity {
     @Override
     public void onStart() {
         super.onStart();
-        final Runnable runTestsTask = new Runnable() {
-            @Override
-            public void run() {
-                nativeRunTests(getCommandLineFlags(), "", getStdoutFilePath());
-                finish();
-            }
-        };
+
+        Path stdoutFilePath = Paths.get(OUTPUT_DIRECTORY).resolve(STDOUT_FILENAME);
+
+        try {
+            Files.deleteIfExists(stdoutFilePath);
+        } catch (IOException e) {
+            Log.e(TAG, "Failed to delete stdout file '" + stdoutFilePath + "': ", e);
+            // Exit the test, since the log file is inaccessible.
+            throw new AssertionError("Failed to delete stdout file '" + stdoutFilePath + "'", e);
+        }
+
+        final Runnable runTestsTask =
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        nativeRunTests(getCommandLineFlags(), "", stdoutFilePath.toString());
+                        finish();
+                    }
+                };
         final Handler handler = new Handler();
         final Runnable startTestThreadTask = new Runnable() {
             @Override
